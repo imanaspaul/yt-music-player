b5bc5e60dc05927ca4b41b4f2f6ce68450db8215
Manas Paul <manas@codelogicx.com>
2021-03-13 18:32:02 +0530

    Player added

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,99 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app" class="container py-5">
+    <div class="row">
+      <div class="col-sm-12 player">
+        <video-wrapper :height="auto" :width="width" ref="player" :player="'youtube'" :videoId="videoId" @play="Yplaying" />
+        <div class="placeholder">
+          <!-- <img  src="https://www.flaticon.com/svg/vstatic/svg/2907/2907253.svg?token=exp=1615639655~hmac=4b7023a5210e57edf4bb5116d73fe5e1" width="100%" height="200" alt="placeholder"> -->
+           <img width="200" class="thumnail" :src="thumnail">
+        </div>
+      </div>
+      <div class="col-sm-12">
+        <input type="url" v-model="url" class="form-control" placeholder="Enter YouTube URL">
+        <div class="container py-5">
+          <div class="row py-5">
+            <div class="controls text-center">
+              <i class="far fa-play-circle" v-if="!playing" @click="play"></i>
+              <i class="fas fa-pause-circle" v-else @click="pause"></i>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      videoId: "8wzTafXWtLE",
+      width: "100%",
+      url: "",
+      thumnail: `https://img.youtube.com/vi/8wzTafXWtLE/0.jpg`,
+      playing: false
+    }
+  },
+  watch: {
+    url() {
+      const url = new URL(this.url)
+      const id = url.pathname.split("/")[1]
+      this.videoId = id
+      this.thumnail = `https://img.youtube.com/vi/${id}/0.jpg`
+    }
+  },
+  methods: {
+    play(){
+      this.$refs.player.player.playVideo();
+    },
+    pause() {
+      this.$refs.player.player.pauseVideo();
+      this.playing = false
+    },
+    Yplaying() {
+      this.playing = true
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+button.btn.btn-danger.mr-4.play {
+  margin-right: 20px;
+}
+.col-sm-12.player {
+  position: relative;
+}
+
+/* .col-sm-12.player::after {
+  content: "";
+  width: 100%;
+  height: 100%;
+  background-color: white;
+  position: absolute;
+  top: -1px;
+  left: 0;
+} */
+.placeholder {
+  width: 100%;
+  height: 100%;
+  background-color: white;
+  position: absolute;
+  top: -1px;
+  left: 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.fa, .far, .fas {
+  font-family: "Font Awesome 5 Free";
+  font-size: 42px;
+  margin: 0px 20px;
+}
+.thumnail {
+  border-radius: 60%;
+  height: 200px;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -2,7 +2,9 @@ import Vue from 'vue'
 import App from './App.vue'
 
 Vue.config.productionTip = false
+import VueVideoWrapper from 'vue-video-wrapper'
 
+Vue.use(VueVideoWrapper) 
 new Vue({
   render: h => h(App),
 }).$mount('#app')
