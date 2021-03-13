c65bac669bd42b50d2b91bc6ade7bdbad110df16
Manas Paul <manas@codelogicx.com>
2021-03-13 18:32:55 +0530

    pwa added

--- src/main.js
+++ src/main.js
@@ -3,8 +3,9 @@ import App from './App.vue'
 
 Vue.config.productionTip = false
 import VueVideoWrapper from 'vue-video-wrapper'
+import './registerServiceWorker'
 
-Vue.use(VueVideoWrapper) 
+Vue.use(VueVideoWrapper)
 new Vue({
   render: h => h(App),
 }).$mount('#app')
